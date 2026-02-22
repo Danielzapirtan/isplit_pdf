cef54073639bc2e39c984deda6069e3822c617fc
Danielzapirtan <antoniudanielzapirtan@gmail.com>
2026-02-22 13:37:01 +0000

    m

--- backup.sh
+++ backup.sh
@@ -4,7 +4,7 @@ cd /content/drive/MyDrive/
 DATE=$(date +6%m%d_%H%M)
 PROJECT=split_chapters
 echo "Compressing ... please wait"
-sleep 20
+wait
 tar -C . -czf $PROJECT_$DATE.gz $PROJECT
 sleep 20
 rm $PROJECT
