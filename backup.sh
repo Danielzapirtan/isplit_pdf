fb05d75ae5f68dba56fddfd0936f6bc373f47a73
Danielzapirtan <137394552+Danielzapirtan@users.noreply.github.com>
2026-02-23 21:11:05 +0200

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -4,10 +4,8 @@ cd /content/drive/MyDrive/
 DATE=$(date +6%m%d_%H%M)
 PROJECT=split_chapters
 echo "Compressing ... please wait"
-wait
+sleep 300
 tar -C . -czf ${PROJECT}_$DATE.tar.gz $PROJECT
-sleep 20
-wait
 #rm -rf $PROJECT
 echo "Compressed ok"
 echo "Open Google Drive and download ${PROJECT}_$DATE.tar.gz"
